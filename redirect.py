7ace41563bd717a0a3c223f1193a327a86e9ef4c
Celsoft <maksim_blg@mail.ru>
2020-10-13 15:18:40 +0300

    Initial commit

---
+++ redirect.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import logging
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+DOMAIN = "https://aparatov-net.appspot.com"
+
+class AllHandler(webapp.RequestHandler):
+    def get(self):
+        #logging.info("INFO --- %s", self.request.path)
+        self.redirect(DOMAIN + self.request.path, True)
+
+application = webapp.WSGIApplication([('/.*', AllHandler)])
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
