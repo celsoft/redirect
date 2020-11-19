577271afe6b278ff25da70805baa7368e4a7500b
Celsoft <maksim_blg@mail.ru>
2020-11-19 10:49:00 +0300

    new

--- redirect.py
+++ redirect.py
@@ -5,7 +5,7 @@ import logging
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-DOMAIN = "https://apparatov.azurewebsites.net"
+DOMAIN = "https://aparatov-net.cf"
 
 class AllHandler(webapp.RequestHandler):
     def get(self):
