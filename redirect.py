145247b916ac3baaecdb88bbe9add0b06fb3ae85
celsoft <maksimblg@gmail.com>
2021-02-05 18:00:25 +0300

    igrovie-automati-na-dengi.azurewebsites.net

--- redirect.py
+++ redirect.py
@@ -5,7 +5,7 @@ import logging
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-DOMAIN = "https://joycasino-oficialniy-sayt.azurewebsites.net"
+DOMAIN = "https://igrovie-automati-na-dengi.azurewebsites.net"
 
 class AllHandler(webapp.RequestHandler):
     def get(self):
